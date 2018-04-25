ac018b14d116515e06e388cb097e5ffacc528269
Eugene Yevtushenko <freeman.zhenia@gmail.com>
2018-04-25 13:42:47 +0200

    Cleaned up BitmapEditor and its tests

--- spec/bitmap_editor_spec.rb
+++ spec/bitmap_editor_spec.rb
@@ -4,14 +4,14 @@ describe BitmapEditor do
   subject(:bitmap_editor) { described_class.new }
 
   it 'should print an error if file doesnt exist' do
-    expect { bitmap_editor.run(nil) }.to output("please provide correct file\n").to_stdout
+    expect { subject.run(nil) }.to output("please provide correct file\n").to_stdout
   end
 
   it 'should print an error if file starts with non-char symbol' do
-    expect { bitmap_editor.run('spec/fixtures/incorrect_content.txt') }.to output("unrecognised command :(\n").to_stdout
+    expect { subject.run('spec/fixtures/incorrect_content.txt') }.to output("unrecognised command :(\n").to_stdout
   end
 
   it 'should pass if file has correct command' do
-    expect { bitmap_editor.run('spec/fixtures/correct_content.txt') }.to output("There is no image\n").to_stdout
+    expect { subject.run('spec/fixtures/correct_content.txt') }.to output("There is no image\n").to_stdout
   end
 end
